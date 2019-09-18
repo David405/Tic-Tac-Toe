2698f0a765555a28e0746feda127d92f3a286cd0
Bruna <brunagenz92@gmail.com>
2019-09-18 15:00:03 -0300

    minor changes

--- lib/helper.rb
+++ lib/helper.rb
@@ -4,26 +4,27 @@ class Board
     def initialize
         @WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]]
         @board = [[0,1,2],[3,4,5],[6,7,8]]
-end
+    end
 
-def display_board
-    @board.each do |i|
-        i.each do |j|
-            print "| #{j} |"
-        end
-        puts ""
-end
+    def display_board
+        @board.each do |i|
+            i.each do |j|
+                print "| #{j} |"
+            end
+            puts ""
+    end
 
-def input_to_index
-    user_input_index = user_input.to_i - 1
-end
+    def input_to_index()
+        user_input_index = user_input.to_i - 1
+    end
 
-def move(board, user_input_index, user_input)
-    board
-    user_input_index = value
-    user_input = []
-end
+    def move(board, user_input_index, user_input)
+        board
+        user_input_index = value
+        user_input = []
+    end
 
-def position_taken?
+    def position_taken?
 
+    end
 end
