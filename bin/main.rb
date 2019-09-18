6c1133006ea4dfc81206adffe9ff91f8e67ba61d
Bruna <brunagenz92@gmail.com>
2019-09-18 13:41:16 -0300

    user interface mock game created

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,20 @@
-print "Hello world!"
+#basic steps
+p "Player One, please, make a move."
+player_1_input = gets.chomp
+p "Move completed!"
+p "Player Two, please, make a move."
+player_2_input = gets.chomp
+p "Move completed"
+
+#if a player tries to make a move to an occupied space
+p "Player One, please, make a move."
+player_1_input = gets.chomp
+p "This space is already occupied!"
+
+#repeat basic steps until a player wins
+
+#when a player wins
+p "Player One wins!"
+
+#if game ties
+p "It's a tie."
