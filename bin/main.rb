b4495bf6274e324b0d74ae97522e61512f853d48
David405 <davidasamonye@gmail.com>
2019-09-23 17:02:02 +0100

    made changes required by the TSE

--- bin/main.rb
+++ bin/main.rb
@@ -1,20 +1,44 @@
-#basic steps
-p "Player One, please, make a move."
+#basic steps 
+p "Player One, please, make a move"
 player_1_input = gets.chomp
 p "Move completed!"
+
+#Print board with registered move
+p "@board"
+
 p "Player Two, please, make a move."
 player_2_input = gets.chomp
 p "Move completed"
+
+#Print board with registered move
+p "@board"
+
+
+#The above steps would be repeated
+p "Player One, make another move"
+
+#Print board with registered move
+p "@board"
+
+p "Player Two, make another move"
+
+#Print board with registered move
+p "@board"
 
 #if a player tries to make a move to an occupied space
 p "Player One, please, make a move."
 player_1_input = gets.chomp
 p "This space is already occupied!"
 
-#repeat basic steps until a player wins
+#if a player enters a move that matches the @WIN_COMBINATION
+p "${Player} won!"
 
-#when a player wins
-p "Player One wins!"
+#Print board with registered move
+p "@board"
 
-#if game ties
-p "It's a tie."
+#if all spaces are occupied without a @WIN_COMBINATION
+p "Match is a draw"
+
+#Print board with registered move
+p "@board"
+
--- lib/helper.rb
+++ lib/helper.rb
@@ -4,7 +4,7 @@ class Board
     def initialize
         @WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[0,4,8]]
         @board = [[0,1,2],[3,4,5],[6,7,8]]
-end
+    end
 
 def display_board
     @board.each do |i|
@@ -23,7 +23,4 @@ def move(board, user_input_index, user_input)
     user_input_index = value
     user_input = []
 end
-
-def position_taken?
-
 end
