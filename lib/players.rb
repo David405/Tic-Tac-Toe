0381d7988c733c53079168d83d7bee2ffe60aacd
David405 <davidasamonye@gmail.com>
2019-10-01 18:13:50 +0100

    fixed linter errors

--- lib/players.rb
+++ lib/players.rb
@@ -1,25 +1,25 @@
-class Player 
-    require_relative 'board'
+class Player
+  require_relative 'board'
 
-    attr_reader :name, :marker
+  attr_reader :name, :marker
 
-    def initialize(mark, marker)
-        @name = name
-        @marker = marker
+  def initialize(mark, marker)
+    @name = name
+    @marker = marker
+  end
+
+  def move(board)
+    position = get_move(board)
+    board.update_board(position.to_i, self)
+  end
+
+  def get_move(board)
+    puts "#{name}, choose a position to place your #{marker}"
+    position = gets.chomp
+
+    until position =~ /^[0-8]{1}$/ && (board.board[position.to_i] != 'x' && board.board[position.to_i] != 'o')
+    puts 'Enter a number that is valid'
+    position = gets.chomp
     end
-
-    def move(board)
-        position = get_move(board)
-        board.update_board(position.to_i, self)
-    end
-
-    def get_move(board)
-        puts "#{name}, choose a position to place your #{marker}"
-        position = gets.chomp
-
-        until position =~ /^[0-8]{1}$/ && (board.board[position.to_i] != 'x' && board.board[position.to_i] != 'o')
-            puts 'Enter a number that is valid'
-            position = gets.chomp
-        end
-    end
+  end
 end
